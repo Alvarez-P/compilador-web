f1b5282bdf235a8050b7c914db7be87c438a6c4d
EstebanBrito <esteban.brito.borges@gmail.com>
2020-09-26 19:33:47 -0500

    Done coding whileHandler

--- js/compiler/matchers/while.js
+++ js/compiler/matchers/while.js
@@ -1,36 +1,51 @@
-import { Token, TokenError } from '../classes/token'
-import { matcherLexeme } from '../matchers/index'
+import { TokenError } from '../classes/token.js'
+import { matcherLexeme } from '../matchers/index.js'
 
 
 const whileHandler = (context) => {
     return (lexeme) => {
         let token = null
         // An. sintacico
-        token = matcherLexeme(lexeme)
+        token = matcherLexeme(lexeme, context)
         //Muy importante esta condicion, NO es igual a if(token instanceof Token)
         if (!token instanceof TokenError){
-            if (!['ID', 'OP'].includes(token.token)) { // Solo analiza ID y OP
+            if (!['ID'].includes(token.token)) { // Solo analiza ID
                 // An. semántico
-                switch(token.token){ // BUG: Falta regex para while
+                switch(token.token){
                     case 'ID':
-                        //If exists in scope, keep. If no, check if operation is active. If yes, deactivate it
-                        //If exist
-                        break
-                    case 'OP':
+                        const prevToken = contex.findVariable(token.lexeme)
+                        if (!prevToken){
+                            const desc = 'Indefinida la variable'
+                            token = new TokenError(token.token, token.lexeme, null, null, desc)
+                        } else if (context.onOperation){
+                            const opDataType = context.operationDataType
+                            if (opDataType!=='any' && opDataType!==prevToken.dataType){
+                                const desc = 'Incompatibilidad de tipos'
+                                token = new TokenError(token.token, token.lexeme, null, null, desc)
+                            }
+                        }
                         break
                 }
             }
         }
+        //Cambiar estado del context
+        if (context.expectedTokens.includes('ID') && context.onOperation) context.onOperation = false
+        if (context.expectedTokens.includes('OA') && token.token!=='DELSE') {
+            const dtype = context.lastToken.dataType
+            if (!dtype) context.operationDataType = 'any'
+            else context.operationDataType = dtype
+        }
 
         //Cambiar token esperado
-        if (context.expectedToken.includes('WHILE')) context.setExpectedToken(['DELSO'])
-        else if (context.expectedToken.includes('DELSO')) context.setExpectedToken(['ID', 'DELSE'])
+        if (context.expectedTokens.includes('WHILE')) context.expectedTokens = ['DELSO']
+        else if (context.expectedTokens.includes('DELSO')) context.expectedTokens = ['ID', 'DELSE']
         //BUG: Si el token esperado era DELSE y recibe "))" o similar, todo el analisis falla.
-        else if (context.expectedToken.includes('DELSE') && token.token ==='DELSE') context.setExpectedToken(['DELBO'])
-        else if (context.expectedToken.includes('ID')) context.setExpectedToken(['OP', 'DELSE'])
-        else if (context.expectedToken.includes('OP')) context.setExpectedToken(['ID'])
+        else if (context.expectedTokens.includes('DELSE') && token.token ==='DELSE') context.expectedTokens = ['DELBO']
+        else if (context.expectedTokens.includes('ID')) context.expectedTokens = ['OA', 'DELSE']
+        else if (context.expectedTokens.includes('OA')) context.expectedTokens = ['ID']
 
         //Retornar
+        context.lastToken = token
         return token
     }
 }
