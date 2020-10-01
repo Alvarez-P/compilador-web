d5101c8d932cf45b56db256ce6c5cf0a67355b62
EstebanBrito <esteban.brito.borges@gmail.com>
2020-09-30 23:06:51 -0500

    Changed operationHandler test structure

--- js/tests/operationHandler.test.js
+++ js/tests/operationHandler.test.js
@@ -1,14 +1,67 @@
 // Tests Operation Handler
 import Context from '../compiler/classes/context.js'
 import { operationHandler } from '../compiler/matchers/operations.js'
-import { scopes } from "./index.js";
+//import { scopes } from "./index.js";
 
 const codeLines = [
     ['a', '=', 'b', '-', 'd'],
     ['b', '=', 'd', '+', '2'],
-    ['int', 'variable', '0', 'a', '+', 'b', '-', '23']
+    ['int', 'variable', '=', 'a', '+', 'b', '-', '23']
 ]
 
+const scopes = [
+    [
+        { token: 'ID', lexeme: 'a', dataType: 'int' },
+        { token: 'ID', lexeme: 'b', dataType: 'int' }
+    ],
+    [
+        { token: 'ID', lexeme: 'a', dataType: 'int' },
+        { token: 'ID', lexeme: 'b', dataType: 'int' },
+        { token: 'ID', lexeme: 'c', dataType: 'boolean' },
+        { token: 'ID', lexeme: 'd', dataType: 'char' }
+    ],
+    [
+        { token: 'ID', lexeme: 'a', dataType: 'int' },
+        { token: 'ID', lexeme: 'b', dataType: 'boolean' }
+    ],
+]
+
+const contexts = scopes.map(scope => {
+    let c = new Context()
+    c.scope = [scope]
+    c.expectedTokens = ['ID', 'TDV'] //MUY IMPORTANTE EL ORDEN DEL ARRAY
+    return c
+})
+
+const handlers = contexts.map(context => {
+    const handler = operationHandler(context)
+    return handler
+})
+
+const showContext = ({ expectedTokens }) => expectedTokens
+
+function test(){
+    const results = []
+    for(let key = 0; key < codeLines.length; key++){
+        const line = codeLines[key], handler = handlers[key]
+
+        const result = []
+        for (const lexeme of line){
+            result.push(handler(lexeme))
+        }
+        results.push(result)
+    }
+
+    for(const key in results){
+        console.log(`RESULT # ${key}`)
+        console.log(results[key])
+        console.log(showContext(contexts[key]))
+    }
+}
+
+test()
+
+/*
 const test = () => {
     for (const key in codeLines) {
         const line = codeLines[key]
@@ -29,4 +82,4 @@ const test = () => {
     }
 }
 
-test()
+test()*/
