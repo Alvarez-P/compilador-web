20f688918b44f367ad22449e8084f5ea10741f44
EstebanBrito <esteban.brito.borges@gmail.com>
2020-10-01 21:25:56 -0500

    Bug fix: while lexemes were not shown as IT

--- js/compiler/recorder/index.js
+++ js/compiler/recorder/index.js
@@ -5,15 +5,15 @@ const register = (tokens, errors, lexemes, tokenCounter, errorTokenCounter) => (
         if(current.lexema === token.lexema && current.token === token.token) isRegistered = true
     })
     //Traducción de tipos específicos a tipos genéricos
-    let realToken = token.token
-    if (['TDV', 'TDF'].includes(token.token)) realToken = 'TD'
-    if (['DELSO', 'DELSE', 'DELBO', 'DELBE'].includes(token.token)) realToken = 'DEL'
-    if (['WHILE'].includes(token.token)) realToken = 'IT'
-    //console.log(token.token, realToken, tokenCounter[realToken], errorTokenCounter[realToken], isLastLexemeInLine)
+    if (['TDV', 'TDF'].includes(token.token)) token.token = 'TD'
+    if (['DELSO', 'DELSE', 'DELBO', 'DELBE'].includes(token.token)) token.token = 'DEL'
+    if (['WHILE'].includes(token.token)) token.token = 'IT'
+    //Compruba si es un token de error
     if (token.description) {
         if (token.errorType==='lexical'){
-            token.token = 'ERLX' + token.token + errorTokenCounter['ERLX'+realToken]
-            errorTokenCounter[realToken] ++
+            const prevToken = token.token
+            token.token = 'ERLX' + token.token + errorTokenCounter['ERLX'+token.token]
+            errorTokenCounter['ERLX'+prevToken] ++
         } else{
             //NOTA: Find todos los semánticos
             token.token = 'ERSEM' + errorTokenCounter['SEM']
@@ -21,9 +21,10 @@ const register = (tokens, errors, lexemes, tokenCounter, errorTokenCounter) => (
         }
         errors.push(token)
     } else if (!isRegistered) {
-        token.token = token.token + tokenCounter[realToken]//.toString()
+        const prevToken = token.token
+        token.token = token.token + tokenCounter[token.token]
+        tokenCounter[prevToken] ++
         tokens.push(token)
-        tokenCounter[realToken] ++
     }
     lexemes += `${token.token} `
     if (isLastLexemeInLine) lexemes += '\n'
